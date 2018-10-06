fc0e8fda181e52bea99746545f9fe0f468163bdd
Jason Schmitt <schmitty890@gmail.com>
2018-10-06 15:34:59 -0400

    user login

--- public/assets/js/test2.js
+++ public/assets/js/test2.js
@@ -1,1 +1,6 @@
 // console.log('test2');
+//
+$(document).on('click', '#header-mobile__toggle', function() {
+  console.log('ayo');
+  $('.site-wrapper').toggleClass('site-wrapper--has-overlay');
+});
