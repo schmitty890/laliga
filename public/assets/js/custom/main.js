a65f8722f72e55bc56e633c98bb8fd7600635f28
Jason Schmitt <schmitty890@gmail.com>
2018-10-22 17:43:02 -0400

    pulling tablesorter code out of main js and into seperate js file

--- public/assets/js/custom/main.js
+++ public/assets/js/custom/main.js
@@ -1,10 +1,4 @@
 (function($) {
   // Place JavaScript code here...
-  console.log(`testing testing!!! ;) :) !?`);
+  console.log(`testing testing!!! :) !`);
 })(jQuery);
-
-$(document).ready(function()
-  {
-    $("#table-standings-innercitydgk-js, #table-standings-yuppyville-js").tablesorter({sortList: [[1,1], [1]]}); // this sorts the second column (table header - the wins column), in ascending order to display the highest wins in first and so on
-  }
-);
---
+++ public/assets/js/custom/standings-widget.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function()
+  {
+    $("#table-standings-innercitydgk-js, #table-standings-yuppyville-js").tablesorter({sortList: [[1,1], [1]]}); // this sorts the second column (table header - the wins column), in ascending order to display the highest wins in first and so on
+  }
+);
