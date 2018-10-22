672b9c1fc4ac8e45d8bc3727a9cf16c0f039f0e7
Jason Schmitt <schmitty890@gmail.com>
2018-10-22 16:54:59 -0400

    adding comment to standings sorttable code

--- public/assets/js/custom/main.js
+++ public/assets/js/custom/main.js
@@ -5,6 +5,6 @@
 
 $(document).ready(function()
   {
-    $("#table-standings-innercitydgk-js, #table-standings-yuppyville-js").tablesorter({sortList: [[1,1], [1]]});
+    $("#table-standings-innercitydgk-js, #table-standings-yuppyville-js").tablesorter({sortList: [[1,1], [1]]}); // this sorts the second column (table header - the wins column), in ascending order to display the highest wins in first and so on
   }
 );
