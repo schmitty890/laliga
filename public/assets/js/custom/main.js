eb9bb4394163f56bef514ac91f0b844157eec029
Jason Schmitt <schmitty890@gmail.com>
2018-10-22 16:53:40 -0400

    adding sorttable library and implementing sorttable on standings widget

--- public/assets/js/custom/main.js
+++ public/assets/js/custom/main.js
@@ -2,3 +2,9 @@
   // Place JavaScript code here...
   console.log(`testing testing!!! :) !`);
 })(jQuery);
+
+$(document).ready(function()
+  {
+    $("#table-standings-innercitydgk-js, #table-standings-yuppyville-js").tablesorter({sortList: [[1,1], [1]]});
+  }
+);
