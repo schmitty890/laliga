2d70a8476a85c6e5c191575304cc9a058823e42c
Jason Schmitt <schmitty890@gmail.com>
2018-10-26 20:11:57 -0400

    analytics test

--- public/assets/js/analytics/analytics-test.js
+++ public/assets/js/analytics/analytics-test.js
@@ -1,8 +1,13 @@
 
 $(document).on('click', '.header-logo', function() {
-  ga('send', 'event', 'button', 'click', 'main logo desktop');
+  // ga('send', 'event', 'button', 'click', 'main logo desktop');
+  ga('send', 'event', {
+    eventCategory: 'logo click',
+    eventAction: 'click',
+    eventLabel: event.target.href
+  });
 });
 
-$(document).on('click', '.header-mobile__logo-img', function() {
-  ga('send', 'event', 'button', 'click', 'main logo mobile');
-});
+// $(document).on('click', '.header-mobile__logo-img', function() {
+//   ga('send', 'event', 'button', 'click', 'main logo mobile');
+// });
